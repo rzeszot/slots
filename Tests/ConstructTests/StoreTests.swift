fc2d46316b170274f4e48e3af711d76bf05e6746
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-10-08 01:39:37 +0200

    add tests

--- Tests/ConstructTests/StoreTests.swift
+++ Tests/ConstructTests/StoreTests.swift
@@ -8,11 +8,11 @@ struct StoreTests {
     func initialize_empty() {
         let sut = Store.empty
 
-        #expect(sut.data.isEmpty)
+        #expect(sut.data.count == 0)
     }
 
     @Test
-    func test() {
+    func find_returns_proper_object() {
         let sut = Store.empty.appending(Foo.self) { foo in
             Text(verbatim: foo.parameter)
         }
@@ -24,6 +24,16 @@ struct StoreTests {
         #expect(sut.find(Foo.self)?(Foo(parameter: "foo")) != nil)
     }
 
+    @Test
+    func appending_creates_new_instance() {
+        let original = Store.empty
+        let sut = original.appending(Foo.self) { foo in
+            Text(verbatim: foo.parameter)
+        }
+
+        #expect(original.data.count == 0)
+        #expect(sut.data.count == 1)
+    }
 }
 
 private struct Foo {
