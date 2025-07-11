a8f9b4e3da3b4ef7a3c62f533daffba11ded8f4b
Damian Rzeszot <damian.rzeszot@gmail.com>
2025-07-11 15:13:22 +0200

    add tests

---
+++ Tests/ConstructTests/ConstructItemTests.swift
@@ -0,0 +1,28 @@
+@testable import Construct
+import SwiftUI
+import Testing
+
+@Suite struct ConstructItemTests {
+    @Test func initializer_missing_1() {
+        let sut = ConstructItem.missing { _ in }
+        #expect(sut.symbol.name == "MissingSymbol.Type")
+    }
+
+    @Test func initializer_missing_2() {
+        let sut = ConstructItem.missing {}
+        #expect(sut.symbol.name == "MissingSymbol.Type")
+    }
+
+    // MARK: -
+    @Test func initializer_some_1() {
+        let sut = ConstructItem(for: SomeSymbol.self) { _ in }
+        #expect(sut.symbol.name == "SomeSymbol.Type")
+    }
+
+    @Test func initializer_some_2() {
+        let sut = ConstructItem(for: SomeSymbol.self) {}
+        #expect(sut.symbol.name == "SomeSymbol.Type")
+    }
+}
+
+private enum SomeSymbol {}
